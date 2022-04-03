5fd1ea83d162cdaa0a77d8180809b2df27c8f43b
euiraekim <harrykur139@gmail.com>
2022-04-03 13:03:43 +0900

    yolo_x_livecell config 수정

--- configs/detection/yolox_x_livecell.py
+++ configs/detection/yolox_x_livecell.py
@@ -110,14 +110,14 @@ data = dict(
 # default 8 gpu
 optimizer = dict(
     type='SGD',
-    lr=0.01,
+    lr=0.01 / 64,
     momentum=0.9,
     weight_decay=5e-4,
     nesterov=True,
     paramwise_cfg=dict(norm_decay_mult=0., bias_decay_mult=0.))
 optimizer_config = dict(grad_clip=None)
 
-max_epochs = 300
+max_epochs = 100
 num_last_epochs = 15
 resume_from = None
 interval = 1
@@ -170,10 +170,10 @@ log_config = dict(
 
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = None
+load_from = data_root + 'checkpoints/yolo_x_coco.pth'
 workflow = [('train', 1)]
 
-# disable opencv multithreading to avoid system being overloaded
-opencv_num_threads = 0
+# # disable opencv multithreading to avoid system being overloaded
+# opencv_num_threads = 0
 # set multi-process start method as `fork` to speed up the training
 mp_start_method = 'fork'
